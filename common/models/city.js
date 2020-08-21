7893f8a2b237347f051dec84099cdff2ebc43c0d
Binh Nguyen <ntbinh100488@gmail.com>
2020-08-21 14:09:36 +0700

    City remote method

--- common/models/city.js
+++ common/models/city.js
@@ -1,5 +1,28 @@
 'use strict';
 
 module.exports = function(City) {
+    City.ed_pop = function (count, cb) {
+        console.log(count);
 
+        var response = {};
+        if(count < 0){
+            response = {
+                statusCode: "400",
+                message: "Count less than zero"
+            };
+        }else{
+            response = {
+                statusCode: "200",
+                message: `Count ${count} less than zero`
+            };
+        }
+
+        return cb(response);
+    }
+
+    City.remoteMethod('ed_pop', {
+        http: { path: '/ed_pop', verb: 'post' },
+        accepts: { arg: 'count', type: 'number', required: true, http: { source: 'body' } },
+        returns: { arg: 'response', type: 'object' }
+    });
 };
