8962b76c11f96a65939f0d50068ab5dd2ed66e4d
Binh Nguyen <ntbinh100488@gmail.com>
2020-09-02 14:39:16 +0700

    add note, data configs

--- server/migrations/create-lb-tables.js
+++ server/migrations/create-lb-tables.js
@@ -1,6 +1,8 @@
 var server = require('../server');
     var ds = server.dataSources.mysql;
-    var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'City', 'region', 'CoffeeShop', ];
+    // var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'City', 'region', 'CoffeeShop', 'note'];
+    var lbTables = ['note'];
+
     ds.automigrate(lbTables, function(er) {
         if (er) throw er;
         console.log('Loopback tables [' - lbTables - '] created in ', ds.adapter.name);
