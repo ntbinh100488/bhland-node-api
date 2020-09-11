70783ba754b9b2a8dfa267d3ac6be4a49ca0057e
Binh Nguyen <ntbinh100488@gmail.com>
2020-09-11 22:01:58 +0700

    ward bulk insert

--- common/models/city.js
+++ common/models/city.js
@@ -1,5 +1,39 @@
 'use strict';
 
 module.exports = function(City) {
+    City.bulkCreate = function(cityData, cb){
+        let err = {};
+        let response = {};
+        
+        console.log(JSON.stringify(cityData));
+        // [{"name":"An Giang", "cityType": "Tỉnh"}, {"name":"Hồ Chí Minh", "cityType": "Thành Phố"}]
 
+        let createdCities = [];
+        cityData.forEach(createCityObj => {
+            const createdCity = City.create(createCityObj);
+            createdCities.push(createdCity);
+        });
+
+        response.status = 200;
+        response.error = {};
+        response.data = createdCities;
+        console.log('response: ' + JSON.stringify(response));
+        // response: {"status":200,"data":[{"isFulfilled":false,"isRejected":false},{"isFulfilled":false,"isRejected":false}]}
+
+        // try {
+        //     if(cb){
+        //         return cb(response);
+        //     }
+        //     return response;
+        // } catch (error) {
+        //     console.log('error: ' + JSON.stringify(error));
+        // }
+        return cb(response);
+    }
+
+    City.remoteMethod('bulkCreate', {
+        http: { path: '/bulkCreate', verb: 'post' },
+        accepts: { arg: 'cityData', type: 'array', required: true, http: { source: 'body' } },
+        returns: { arg: 'response', type: 'object' }
+    });
 };
--- common/models/ward.js
+++ common/models/ward.js
@@ -1,5 +1,38 @@
 'use strict';
 
 module.exports = function(Ward) {
+    Ward.bulkCreate = function(wardDatas, cb){
+        let err = {};
+        let response = {};
+        
+        console.log(JSON.stringify(wardDatas));
 
+        let createdWards = [];
+        wardDatas.forEach(createWardObj => {
+            const createdWard = Ward.create(createWardObj);
+            createdWards.push(createdWard);
+        });
+
+        response.status = 200;
+        response.error = {};
+        response.data = createdWards;
+        console.log('response: ' + JSON.stringify(response));
+        // response: {"status":200,"data":[{"isFulfilled":false,"isRejected":false},{"isFulfilled":false,"isRejected":false}]}
+
+        // try {
+        //     if(cb){
+        //         return cb(response);
+        //     }
+        //     return response;
+        // } catch (error) {
+        //     console.log('error: ' + JSON.stringify(error));
+        // }
+        return cb(response);
+    }
+
+    Ward.remoteMethod('bulkCreate', {
+        http: { path: '/bulkCreate', verb: 'post' },
+        accepts: { arg: 'wardData', type: 'array', required: true, http: { source: 'body' } },
+        returns: { arg: 'response', type: 'object' }
+    });
 };
