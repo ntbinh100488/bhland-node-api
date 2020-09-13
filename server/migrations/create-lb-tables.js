4c309056dc326d5ab7a4aa01cdc3f7ffaaa62df9
Binh Nguyen <ntbinh100488@gmail.com>
2020-09-13 07:46:23 +0700

    commit

--- server/migrations/create-lb-tables.js
+++ server/migrations/create-lb-tables.js
@@ -1,8 +1,6 @@
 var server = require('../server');
     var ds = server.dataSources.mysql;
-    // var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'note', 'staff'];
-    // var lbTables = ['city', 'district', 'ward'];
-    var lbTables = ['realestate'];
+    var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'note', 'staff', 'city', 'district', 'ward', 'realestate'];
 
     ds.automigrate(lbTables, function(er) {
         if (er) throw er;
