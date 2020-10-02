01de7bb6b46447c5d0200be31f46c54d379c237b
Binh Nguyen <ntbinh100488@gmail.com>
2020-10-02 15:37:12 +0700

    customer group

--- server/migrations/create-lb-tables.js
+++ server/migrations/create-lb-tables.js
@@ -3,7 +3,8 @@ var server = require('../server');
     // var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'note', 'staff', 'city', 'district', 'ward', 'realestate'];
     // var lbTables = ['liabilitystatus', 'liabilitytype', 'liability'];
     // var lbTables = ['customer', 'customerbudget', 'customertype'];
-    var lbTables = ['customer'];
+    // var lbTables = ['customer'];
+    var lbTables = ['customergroup'];
 
     ds.automigrate(lbTables, function(er) {
         if (er) throw er;
